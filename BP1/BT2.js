f72272e89b76da5afa71dbde1d949a2d43e3ae71
Your Name <you@example.com>
2023-02-13 11:43:07 +0700

    BT2 và BT3 và BT4 sửa code có sẵn

--- BP1/BT2.js
+++ BP1/BT2.js
@@ -1,22 +1,32 @@
 const inventory = [
-
     {type: "machine", value: 5000},
-
-
     {type: "machine", value: 650},
-
-
     {type: "duck", value: 10},
-
-
     {type: "furniture", value: 1200},
-
-
     {type: "machine", value: 77}
-
-
 ]
+let tong = 0
+
+let inventoryItem = inventory.filter((item)=>{
+    if(item.type === 'machine'){
+        return item
+    }
+})
 
 
-let totalMachineValue = //VIẾT_BIỂU_THỨC_TẠI_ĐÂY
-    console.log(totalMachineValue)
+// Cach 1 : dung map, khong reduce
+ inventoryItem.map((item)=>{
+    return tong += item.value
+})
+console.log(tong)
+
+
+// Cach 2 : dung reduce va map
+
+let result = inventoryItem.map((item)=>{
+    // console.log(item)
+    return item.value
+}).reduce((current,total)=>{
+    return current + total
+})
+console.log(result);

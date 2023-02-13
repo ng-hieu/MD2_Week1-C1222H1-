1f07f8b713d919078e8a82a25c0952533f1a8afe
Your Name <you@example.com>
2023-02-13 10:06:20 +0700

    destructuring assignment (1)

---
+++ BP1/BT2.js
@@ -0,0 +1,22 @@
+const inventory = [
+
+    {type: "machine", value: 5000},
+
+
+    {type: "machine", value: 650},
+
+
+    {type: "duck", value: 10},
+
+
+    {type: "furniture", value: 1200},
+
+
+    {type: "machine", value: 77}
+
+
+]
+
+
+let totalMachineValue = //VIẾT_BIỂU_THỨC_TẠI_ĐÂY
+    console.log(totalMachineValue)
---
+++ BP1/BT3.js
@@ -0,0 +1,18 @@
+let profile = {
+
+    fname: 'Code',
+
+
+    lname: 'Gym',
+
+
+    bday: new Date('1979-01-02')
+
+
+}
+
+
+let {fname, lname} = profile
+
+console.log(fname);
+console.log(lname);
